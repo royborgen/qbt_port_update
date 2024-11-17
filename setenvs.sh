35d2859bbdef60e45d7b4508511b690f24a665a7
Roy Borgen <roy@borgen.com>
2024-11-17 20:22:50 +0100

    Added optional shell script to set enviroment variables

--- setenvs.sh
+++ setenvs.sh
@@ -4,5 +4,5 @@ export PATH_GLUETUN=/srv/gluetun/forwarded_port
 export PATH_QBITTORRENT=/srv/qBittorrent/qBittorrent.conf
 export QBT_CONTAINER_ID=qbittorrent
 export CREATE_LOG_FILE=yes
-export LOGFILE=/home/user/logs/qbt_port_update.log
+export LOGFILE=qbt_port_update.log
 export LOGTIMEFORMAT="%d-%m-%Y %H:%M:%S"
